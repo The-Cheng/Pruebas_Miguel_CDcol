b88505ef89db1a06e5b33f1c6f7252a74fd0ce12
miguelcanon27 <39639397+miguelcanon27@users.noreply.github.com>
2018-07-16 08:20:22 -0500

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -15,8 +15,8 @@ product = 'LS8_OLI_LASRC' #Producto sobre el que se hará la consulta (unidad de
 bands=["blue","green","red","nir", "swir1","swir2"] #arreglo de bandas 
 time_ranges = [("2013-01-01", "2013-12-31")] #Una lista de tuplas, cada tupla representa un periodo
 #área sobre la cual se hará la consulta:
-min_long = -75
-min_lat = 5
+min_long = -69
+min_lat = 2
 
 
 dc = datacube.Datacube(app="{}_{}_{}".format(algorithm,version,execID))
