d0c2035f4c1304abd2186dba085944339932db2d
miguelcanon27 <39639397+miguelcanon27@users.noreply.github.com>
2018-08-09 11:54:56 -0500

    Update descarga.py

--- descarga.py
+++ descarga.py
@@ -1,12 +1,1 @@
-import os
-import re
-import xarray as xr
-import numpy as np
-import datacube
-
-#xarr0=xarr[0] 
-#del xarr
-    
-output= xarr0
-
-#xarr0.to_netcdf(str(min_long)+'_'+str(min_lat)+"_salida.nc")
+ output= xarr0
