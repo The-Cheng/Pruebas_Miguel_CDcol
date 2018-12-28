10926fa90e91f465213ce99d8d656049293f04b9
miguelcanon27 <39639397+miguelcanon27@users.noreply.github.com>
2018-12-28 11:32:48 -0500

    Update PCA_mosaico.py

--- PCA_mosaico.py
+++ PCA_mosaico.py
@@ -12,14 +12,7 @@ def isin(element, test_elements, assume_unique=False, invert=False):
     element = np.asarray(element)
     return np.in1d(element, test_elements, assume_unique=assume_unique, invert=invert).reshape(element.shape)
 
-
-
-
-
 nbar = xarr0
-
-
-
 nodata=-9999
 
 validValues=set()
